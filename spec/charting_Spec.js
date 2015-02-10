9fc36e0ff4ccde61d1b43e4e908c8e97ed41d798
Ciprian Ceteras <cipix2000@gmail.com>
2015-02-10 17:51:32 +0200

    small changes

--- spec/charting_Spec.js
+++ spec/charting_Spec.js
@@ -1,7 +1,7 @@
-/* global $, describe, it, xit, after, beforeEach, afterEach, expect, jasmine, spyOn */
+/* global $, describe, it, xit, after, beforeEach, afterEach, expect, jasmine, spyOn, HistoryBuffer */
 /* jshint browser: true*/
 
-describe('Flot charting', function () {
+describe('Flot charting: ', function () {
     'use strict';
 
     var plot;
@@ -18,9 +18,28 @@ describe('Flot charting', function () {
         jasmine.clock().uninstall();
     });
 
-    it('should be possible to specify a cursor when creating the plot', function () {
-        plot = $.plot("#placeholder", [], {});
+    it('should be possible to specify a historyBuffer when creating the plot', function () {
+        var hb = new HistoryBuffer(10, 1);
+        hb.push(33);
+        plot = $.plot("#placeholder", [[]], {
+            series: {
+                historyBuffer: hb
+            }
+        });
 
-        expect(true).toBe(false);
+        expect(plot.getData()[0].data).toEqual([[0, 33]]);
+    });
+
+    it('should be keep track of the total number of elements introduced in the buffer', function () {
+        var hb = new HistoryBuffer(1, 1);
+        hb.push(33);
+        hb.push(34);
+        plot = $.plot("#placeholder", [[]], {
+            series: {
+                historyBuffer: hb
+            }
+        });
+
+        expect(plot.getData()[0].data).toEqual([[1, 34]]);
     });
 });
