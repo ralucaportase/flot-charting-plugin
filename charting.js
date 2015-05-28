3450c78436df92f89f09f0d2e8000e59023b20af
Ciprian Ceteras <cipix2000@gmail.com>
2015-05-28 10:42:57 -0500

    demo tweaks

--- charting.js
+++ charting.js
@@ -4,7 +4,7 @@
 $(function () {
     'use strict';
     var plot;
-    var buffer = new HistoryBuffer(200 * 1024, 3);
+    var buffer = new HistoryBuffer(100 * 1024, 3);
     var globalIndex = 0;
     var chartStep = 0.0001;
 
